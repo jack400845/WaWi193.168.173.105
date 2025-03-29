c4c86a3a7743df84230833fb131ddc466b1a1706
jack400845 <marckor.mk+so@gmail.com>
2025-03-29 18:26:40 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php – MySQL‑Verbindungsdaten
+$host     = "expert-sagan-577.lima-db.de";
+$port     = 3306;
+$dbname   = "db_445211_1";
+$username = "USER445211";
+$password = "pobwod-0siqma-syMmur";
+?>
