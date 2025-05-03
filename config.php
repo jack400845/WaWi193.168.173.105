78b5f6fef6cc552549f847d49afee0b5917f0b33
jack400845 <marckor.mk+so@gmail.com>
2025-05-03 20:03:27 +0200

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config.php – MySQL‑Verbindungsdaten
-$host     = "expert-sagan-577.lima-db.de";
-$port     = 3306;
-$dbname   = "db_445211_1";
-$username = "USER445211";
-$password = "pobwod-0siqma-syMmur";
-?>
